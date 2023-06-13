34daf465e9a8e1e62be17b91903084a167c53a42
kung-kung <mayfire12323@gmail.com>
2023-06-13 16:33:19 +0900

    sulwhasoo 2일 clear

--- js/index.js
+++ js/index.js
@@ -35,5 +35,29 @@ $(function(){
         $(".sc2_b ul").removeClass("act")
         $(".sc2_b ul").eq(x).addClass("act")
     })
-
+    $(".fs_ul").slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true,
+        arrows: true,
+        fade: true
+    })
+    $(".m_ham").click(function(){
+        $(".m_nav_bg").animate({left:0},5,function(){
+            $(".m_nav_bg").delay(50).animate({opacity:1},5)
+        })
+    })
+    $(".m_nav_bg").click(function(){
+        $(".m_nav_bg").animate({left:"-100%"},5,function(){
+            $(".m_nav_bg").animate({opacity:0},5)
+        })
+    })
+    
+    $(".m_main>li").hover(function(){
+        $(this).find("a").addClass("on")
+        $(this).find(".m_sub").stop().slideDown()
+    },function(){
+        $(".m_icon").parent().removeClass("on")
+        $(".m_sub").stop().slideUp()
+    })
 })
